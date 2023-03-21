e6dc3d8161e65a742ffcc032181525985b83fa38
Oscar <oscaralexruiz@gmail.com>
2023-03-21 11:55:47 -0500

    feat(configure): add several steps

--- configure.sh
+++ configure.sh
@@ -4,6 +4,11 @@ name=$1
 email=$2
 download_directory=~/Downloads
 config_directory=~/.config
+dotfiles_config_directory=./config_files
+real_kitty_config_directory=${config_directory}/kitty
+dotfiles_kitty_config=${dotfiles_config_directory}/kitty.conf
+fonts_directory=~/.fonts
+
 
 if [ -z "$name" ] && [ -z "$email" ]
 then
@@ -59,9 +64,57 @@ install_and_configure_nvim(){
 	sudo apt install g++
 }
 
+install_node_and_yarn(){
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+	nvm install --lts
+	npm i -g yarn -y
+}
+
+install_zsh_and_ohmyszh(){
+	sudo apt install zsh -y
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_and_configure_kitty_terminal(){
+	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+	# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+	# your system-wide PATH)
+	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+	# Place the kitty.desktop file somewhere it can be found by the OS
+	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+	# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+	# Update the paths to the kitty and its icon in the kitty.desktop file(s)
+	sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+	sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+
+	mkdir -p $real_kitty_config_directory
+	ln $dotfiles_kitty_config $real_kitty_config_directory
+}
+
+install_jetbrains_font(){
+	font_zip_file=font.zip
+	font_directory=${download_directory}/font
+	font_zip_file_full_path=${download_directory}/${font_zip_file}
+
+	download "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip" $font_zip_file
+	unzip ${font_zip_file_full_path} -d ${font_directory}
+
+	mkdir -p $fonts_directory
+
+	cp ${font_directory}/*.ttf $fonts_directory
+
+	rm -fr $font_zip_file font_directory
+}
+
 sudo apt update
 sudo apt upgrade -y
 
+install_jetbrains_font
+install_zsh_and_ohmyszh
+install_and_configure_kitty_terminal
+install_node_and_yarn
 install_and_configure_git_and_github
 install_and_configure_nvim
 
